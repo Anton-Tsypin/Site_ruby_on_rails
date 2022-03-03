3e7981a637a65272844df97f2012e233c2693a4e
User User <user@vmware.localdomain>
2022-03-03 22:21:27 +0300

    new authentication panels

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,37 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-end
 
-def show
-    @user = User.find(params[:id])
-end
-
-def new
-    @user = User.new
-end
-
-  def create
-    @user = Usere.new(user_params)
-
-    if @user.save
-        redirect_to @user
-    else
-        render :new, status: :unprocessable_entity
+    #http_basic_authenticate_with name: "test", password: "test", except: [:index, :new, :create]
+  
+    def index
+        @users = User.all
     end
-end
 
-private
-    def user_params
-        params.require(:user).permit(:name, :login, :password)
+    def show
+        if params[:id].to_i <=  User.count
+            @user = User.find(params[:id])
+        end
     end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to @user
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def list
+        @users = User.all
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:name, :login, :password, :password_confirmation)
+        end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_27_113727) do
+ActiveRecord::Schema.define(version: 2022_02_27_182118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_buffercache"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_02_27_113727) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_confirmation"
   end
 
 end
