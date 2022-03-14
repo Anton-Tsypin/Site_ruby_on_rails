1a709d73a749ca416e6d189f2bbd49770a3e5d26
User User <user@vmware.localdomain>
2022-03-15 00:29:44 +0300

    custom pages for posts and new routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,18 @@
 class PostsController < ApplicationController
 
+    def index
+        @posts = Post.all
+    end
+
     def create
         @post = Post.create(user_id:session[:user_id], body:post_params[:body], title:post_params[:title])
         redirect_to user_path(session[:user_id])
     end
     
+    def show
+        @post = Post.find_by(id:params[:id])
+    end
+
     private
     def post_params
         params.require(:post).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'users#index'
-  get '/' => 'users#index'
+  root 'posts#index'
+  get '/' => 'posts#index'
 
-  get '/users' => 'users#index'
+  get '/users' => 'users#login'
   get '/users/list' => 'users#list'
+
   get '/users/login' => 'users#login'
   post '/users/login' => 'sessions#create'
+
+  get '/posts' => 'posts#index'
+  get '/posts/:id' => 'posts#show'
   
   resources :users do
     resources :posts
