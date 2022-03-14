2a2675b6d053e9381ae44769fb936346452b28fd
User User <user@vmware.localdomain>
2022-03-14 20:13:13 +0300

    added titles for posts and some design fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
 
     def create
-        @post = Post.create(user_id:session[:user_id], body:post_params[:body])
+        @post = Post.create(user_id:session[:user_id], body:post_params[:body], title:post_params[:title])
         redirect_to user_path(session[:user_id])
     end
     
     private
     def post_params
-        params.require(:post).permit(:body)
+        params.require(:post).permit(:title, :body)
     end
 
 end
